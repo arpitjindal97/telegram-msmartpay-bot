b9ba99192c12c9aa9d0f59aac5539309230f04d0
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-08-09 20:59:46 +0530

    too many syntax error

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,11 @@ task :deploy do
 	on "root@139.59.71.230" do
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) &&
 		if [ \"$DEPLOY_KEY_PASS\" == \"\" ];then 
-			exit 0; 
+			exit 0
 		else 
 			docker stop $docker_cont_id && 
 			docker rm $docker_cont_id && 
-			docker rmi #{fetch(:docker_repo)}; 
+			docker rmi #{fetch(:docker_repo)} 
 		fi"
 		execute "docker pull #{fetch(:docker_repo)} > /dev/null"
 		execute "docker run -d #{fetch(:docker_repo)}"
