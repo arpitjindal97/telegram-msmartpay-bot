39381f6892cfa5c1488622ca93c906fdd292b787
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-08-09 20:20:47 +0530

    fixed capistrano script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,15 @@ set :docker_repo, "arpitjindal1997/telegram-msmartpay"
 
 task :deploy do
 	on "root@139.59.71.230" do
-		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) && docker stop $docker_cont_id && docker rm $docker_cont_id"
-		execute "docker rmi #{fetch(:docker_repo)}"
+		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) &&
+		if [ \"$DEPLOY_KEY_PASS\" == \"\" ];then 
+			exit 0; 
+		else 
+			docker stop $docker_cont_id && 
+			docker rm $docker_cont_id && 
+			docker rmi #{fetch(:docker_repo)}; 
+		fi"
 		execute "docker pull #{fetch(:docker_repo)} > /dev/null"
-		execute "docker run -d #{fecth(:docker_repo)}"
+		execute "docker run -d #{fetch(:docker_repo)}"
 	end
 end
