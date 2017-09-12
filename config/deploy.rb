19e749a7d286180f557451643c01d77b594c10cb
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-12 23:04:51 +0530

    cap misconfigured

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 set :docker_repo, "$DOCKER_USERNAME/telegram-msmartpay"
 
 task :deploy do
-	on "$server-1" do
+	on "#{fetch(:server-1)}" do
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) 
 		if [ \"$docker_cont_id=\" == \"\" ];then exit 0
 		else 	docker stop $docker_cont_id  
