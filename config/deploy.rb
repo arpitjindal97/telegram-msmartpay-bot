cb32e8526beedcdee33fe5deea15717fbce555cb
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-13 23:14:54 +0530

    fixed this time

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,12 @@
-set :docker_repo, "$DOCKER_USERNAME/telegram-msmartpay"
-server ENV["server_1_host"], user: ENV["server_1_user"]
+set :docker_user , ENV["DOCKER_USERNAME"]
+set :docker_repo, "#{fetch(:docker_user)}/telegram-msmartpay"
+
+set :server_host, ENV["server_1_host"]
+set :server_user, ENV["server_1_user"]
+set :server_url, "#{fetch(:server_user)}@#{fetch(:server_host)}"
+
 task :deploy do
-	on roles(:all) do |server|
+	on "#{fetch(:server_url)}" do
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) 
 		if [ \"$docker_cont_id=\" == \"\" ];then exit 0
 		else 	docker stop $docker_cont_id  
