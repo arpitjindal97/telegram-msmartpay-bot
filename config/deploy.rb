f44ab3dffdb4a61c175847b4734616225a2bcb18
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-13 22:01:20 +0530

    configuration fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 set :docker_repo, "$DOCKER_USERNAME/telegram-msmartpay"
-set :server, "$server_1_host"
-set :server_user, "$server_1_user"
-server "#{fetch(:server)}", user: "#{fetch(:server_user)}"
+server ENV["server_1_host"], user: ENV["server_1_user"]
 task :deploy do
 	on roles(:all) do |server|
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) 
