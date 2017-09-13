2af5c58e3f0012b26e0f7de3b8303d7d326bb1c7
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-13 19:28:09 +0530

    configured correctly

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
-
 set :docker_repo, "$DOCKER_USERNAME/telegram-msmartpay"
-
+set :server, "$server_1_host"
+set :server_user, "$server_1_user"
+server "#{fetch(:server)}", user: "#{fetch(:server_user)}"
 task :deploy do
-	on "#{fetch(:server-1)}" do
+	on roles(:all) do |server|
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) 
 		if [ \"$docker_cont_id=\" == \"\" ];then exit 0
 		else 	docker stop $docker_cont_id  
-			docker rm $docker_cont_id  
+			docker rm $docker_cont_id
 			docker rmi #{fetch(:docker_repo)}
 		fi"
 		execute "docker pull #{fetch(:docker_repo)} > /dev/null"
