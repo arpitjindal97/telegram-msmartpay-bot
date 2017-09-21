2524b6d3c41da735542b9e71fdcb67a7337d8867
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-21 15:52:08 +0530

    syntax mistake corrected

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
-set :docker_user , ENV["DOCKER_USERNAME"]
-set :docker_repo, "#{fetch(:docker_user)}/telegram-msmartpay"
+set :docker_repo, ENV["DOCKER_IMG"]
 
 set :server_host, ENV["server_1_host"]
 set :server_user, ENV["server_1_user"]
@@ -8,7 +7,7 @@ set :server_url, "#{fetch(:server_user)}@#{fetch(:server_host)}"
 task :deploy do
 	on "#{fetch(:server_url)}" do
 		execute "export docker_cont_id=$(docker ps -a | grep #{fetch(:docker_repo)} | cut -d ' ' -f 1 ) 
-		if [ \"$docker_cont_id=\" == \"\" ];then exit 0
+		if [ \"$docker_cont_id\" == \"\" ];then exit 0
 		else 	docker stop $docker_cont_id  
 			docker rm $docker_cont_id
 			docker rmi #{fetch(:docker_repo)}
