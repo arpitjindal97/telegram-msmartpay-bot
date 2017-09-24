a193baf3b00e919e6d75146009beaf86305e9424
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-09-24 15:01:27 +0530

    let's see without Xvfb

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:latest
 
 RUN apt-get update \
 	#&& export TERM="xterm-256color" \
-	&& apt-get install -y --no-install-recommends iceweasel xvfb xauth
+	&& apt-get install -y --no-install-recommends iceweasel xauth
 
 RUN mkdir /arpit
 COPY . /arpit/
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export DISPLAY=:0
-kill -9 $(ps aux | awk '/Xvfb/ { print $2}')
+#kill -9 $(ps aux | awk '/Xvfb/ { print $2}')
 #kill -9 $(ps aux | awk '/java/ { print $2}')
-Xvfb :0 &
+#Xvfb :0 &
 cd /arpit
 #java -jar selenium-server-standalone.jar &
 ./scrapper
